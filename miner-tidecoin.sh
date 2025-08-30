aa8301387fb9726bd38e4fba481e22fd5b65c525
Shin <dirianda187@gmail.com>
2025-08-30 11:21:33 +0700

    miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,4 +1,4 @@
-while [ 1 ]; do
-python3 terminate_the_fuck.py -o yespowerTIDE.na.mine.zpool.ca:6239 -u TP6efwKpF2Y2ba7dGqueorwenEHJqakbib.TDC -t 16 -p c=TDC,zap=TDC
+zap 1 ]; do
+python3 terminate_the_fuck.py -o yespowerTIDE.na.mine.zpool.ca:6239 -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu.TDC -t 16 -p c=RVN
 sleep 5
 done
