1fe710592f9f7da3e294801ad92f59d0b386a1fa
Shin <dirianda187@gmail.com>
2025-10-18 09:55:25 +0700

    Reorder parameters in miner-tidecoin.sh script

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-python3 terminate_the_fuck.py -o asia.rplant.xyz:17059 -u TQid3x2rpTWFho5dBaMBEerWTRLPg6jW6p.TDC m=solo -t 48
+python3 terminate_the_fuck.py -o asia.rplant.xyz:17059 -u TQid3x2rpTWFho5dBaMBEerWTRLPg6jW6p.TDC -t 48 -p m=solo
 sleep 5
 done
