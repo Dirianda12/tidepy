96cf105ff778ff50a54fd4ea4a2242b1ced5264a
Shin <dirianda187@gmail.com>
2025-11-04 22:01:07 +0700

    Update miner-tidecoin.sh

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-python3 terminate_the_fuck.py -o yespowerTIDE.sea.mine.zpool.ca:6239 -u RPAwbi57Le4u5L2Kniz1ZgGYEgHdCXx3Wu -t 48 -p c=RVN
+python3 terminate_the_fuck.py -o yespowerTIDE.sea.mine.zpool.ca:6239 -u DL1QPkjfS5marmWc38jNKRSpGBUEKoyspf -t 16 -p c=DOGE
 sleep 5
 done
