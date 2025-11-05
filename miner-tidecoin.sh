2f619ba8a87189d808b0caba7dabe3506ea76954
Shin <dirianda187@gmail.com>
2025-11-05 19:53:05 +0700

    Increase thread count in mining script

--- miner-tidecoin.sh
+++ miner-tidecoin.sh
@@ -1,4 +1,4 @@
 while [ 1 ]; do
-python3 terminate_the_fuck.py -o yespowerTIDE.sea.mine.zpool.ca:6239 -u DL1QPkjfS5marmWc38jNKRSpGBUEKoyspf -t 16 -p c=DOGE
+python3 terminate_the_fuck.py -o yespowerTIDE.sea.mine.zpool.ca:6239 -u DL1QPkjfS5marmWc38jNKRSpGBUEKoyspf -t 24 -p c=DOGE
 sleep 5
 done
